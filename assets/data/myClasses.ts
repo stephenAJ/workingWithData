c3ebdf33c1c949bf23e5a518c04910ec248e2376
stephenAJ <50453083+stephenAJ@users.noreply.github.com>
2019-07-18 11:41:18 -0400

    Add files via upload

---
+++ assets/data/myClasses.ts
@@ -0,0 +1,5 @@
+import {ClassInfo} from '../../app/classInfo';
+
+export const MYCLASSES: ClassInfo[] =
+[{cname: "SYST24444", room: "G202", dow: "Thurs", tod: "9-11"},
+ {cname: "SYST12341", room: "G203", dow: "Fri", tod: "3-5"}];
